56413f4b20116001241427f256521c4f14b70193
Fiona Oh <fionaoh2002@gmail.com>
2021-07-14 21:40:12 -0700

    added image, labels, buttons, and button functionality!

--- Mini Project Example/ViewController.swift
+++ Mini Project Example/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var factLabel1: UILabel!
+    @IBOutlet weak var factLabel2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    //this button works by **changing** the text of factLabel1
+    @IBAction func revealFact1(_ sender: UIButton) {
+        factLabel1.text = "My favorite movie is Arrival!"
+    }
+    
+    //this button works by changing the **hidden status** of factLabel2
+    @IBAction func revealFact2(_ sender: UIButton) {
+        factLabel2.isHidden = false
+    }
+    
 }
 
