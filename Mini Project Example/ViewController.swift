9fca56702926ad63ee34fff6e5e2eaf58b1208d6
Fiona Oh <fionaoh2002@gmail.com>
2021-07-14 21:17:55 -0700

    Initial Commit

---
+++ Mini Project Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Project Example
+//
+//  Created by Fiona Oh on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
